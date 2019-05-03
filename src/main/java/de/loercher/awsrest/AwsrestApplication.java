b47092abc95e528bbd4847b4c3783a03285d7612
OldJimmy <thomas.loercher42@gmail.com>
2019-05-03 15:12:24 +0200

    Initial commit

---
+++ src/main/java/de/loercher/awsrest/AwsrestApplication.java
@@ -0,0 +1,13 @@
+package de.loercher.awsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/loercher/awsrest/AwsrestApplicationTests.java
@@ -0,0 +1,16 @@
+package de.loercher.awsrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsrestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
